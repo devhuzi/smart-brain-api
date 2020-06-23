6c41b1e045241df7df42129e44aa5f1850b695e0
DevHuzi <hux3fa@gmail.com>
2020-06-23 21:22:14 +1000

    fixes

--- controllers/image.js
+++ controllers/image.js
@@ -23,9 +23,10 @@ const handleApiCall = (req, res) => {
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
         .then(data => {
-            res.json(console.log(data));
+            res.json(data);
+            console.log("this is working data is here :", data);
         })
-        .catch(err => res.status(400).json('unable to work with api', err))
+        .catch(err => res.status(400).json('unable to work with api'))
 }
 
 
