f3529bde397f02a0e2c4cb2b2e3caffb4d25f79d
DevHuzi <hux3fa@gmail.com>
2020-06-23 21:35:19 +1000

    fixes

--- server.js
+++ server.js
@@ -23,12 +23,7 @@ const db = knex({
 const app = express();
 
 app.use(express.json());
-//app.use(cors());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
 
 
 app.get('/', (req, res) => {res.send('it is working')});
